6e0ac02b52b5dcf4a52b767f04c66f9e295fbef0
richlink-tech <84175533+richlink-tech@users.noreply.github.com>
2021-09-04 11:11:00 +0800

    Update Master.py

--- SampleCode/BLE/RL62M/Library/Master.py
+++ SampleCode/BLE/RL62M/Library/Master.py
@@ -15,16 +15,15 @@ BLE = RL62M.GATT(uart,role='CENTER')
 # two connect mode ,select one connect to device 
 BLE.ScanConnect() # scan and select the most near device (scan 5sec)
 # BLE.ScanConnect(mac='7002000008B6') # don't need scan , use device mac address connect
-
+ 
 while True: # send / recv data
     m=BLE.RecvData()
     if BLE.state == 'DISCONNECTED':
-        #print('disconnected')
         break
     else :
         BLE.SendData('A')
         if m != None :
-          if m == 'A':
+          if 'A' in m:
             ledy.toggle()
             #print ('recv data is:' , m)
     delay(1000)
