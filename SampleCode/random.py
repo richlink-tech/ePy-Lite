2b20ac579512a9dca59eba9d77b3f87e7caee4eb
richlink-tech <84175533+richlink-tech@users.noreply.github.com>
2021-09-06 14:05:07 +0800

    add random mode function sample

--- SampleCode/random.py
+++ SampleCode/random.py
@@ -1,10 +1,11 @@
+'''
+ePy Lite random number module
+'''
 import urandom as random
-
 '''
 urandom.getrandbits (nbit)
 取nbit 的亂數 , 2^7 = 0~128
 '''
-
 def rand (a,b): # 產生數字a到 b的 亂數 
     return (random.getrandbits(7) %(b-a+1)+ a)
 
@@ -13,3 +14,11 @@ for i in range(0,10):
 
 for i in range(0,10):
     print (rand (2,4)) #取 2到 4亂數
+    
+random.randinit(10,100) # get int number 10~100 random number
+random.random() # get 0 to <1 float random number
+random.randrange (1,7) # get int 1 to 6 random number 
+
+L = [[1,0,0],[0,1,0],[0,0,1],[20,20,0],[30,20,40]]
+
+random.choice (L) # from L list random choice a item
