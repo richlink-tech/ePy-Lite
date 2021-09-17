b50720e58116e3693d2876dd20121bbf543aafbc
richlink-tech <84175533+richlink-tech@users.noreply.github.com>
2021-09-17 19:46:51 +0800

    Update Master.py

--- SampleCode/BLE/RL62M/Library/Master.py
+++ SampleCode/BLE/RL62M/Library/Master.py
@@ -1,5 +1,5 @@
 '''
-Create a Master (Center Mode)
+Create a Master (Central Mode)
 direct scan and connect to first (reference RSSI) device 
 send 'A' to device (1sec)
 and recv 'A' to change (Y)LED status
@@ -11,7 +11,7 @@ uart = UART(1,115200,timeout=200,read_buf_len=512)
 ledy = LED('ledy')
 
 ''' Center mode  sample '''
-BLE = RL62M.GATT(uart,role='CENTER')
+BLE = RL62M.GATT(uart,role='CENTRAL')
 # two connect mode ,select one connect to device 
 BLE.ScanConnect(name_header='EPY_') # scan and select the most near device (scan 5sec)
 # BLE.ScanConnect(mac='7002000008B6') # don't need scan , use device mac address connect
